eef1163cef9cafe6295c56cb5f413b62bc6bf16a
VictorLluva <43237887+VictorLluva@users.noreply.github.com>
2020-06-06 08:39:51 +0200

    Main file
    Main file in Python

--- Logistic Regression numerical.py
+++ Logistic Regression numerical.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
+
+
 # load the dataset
 
 df = pd.read_csv("C:\Games\python_test.csv")
@@ -19,7 +21,7 @@ df = pd.read_csv("C:\Games\python_test.csv")
 X, y = df.values[:, 0:9], df.values[:, 10]
 # ensure all data are floating point values
 X = X.astype('float32')
-# encode strings to integer
+# encode strings to integer 
 y = LabelEncoder().fit_transform(y)
 # split into train and test datasets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
@@ -35,18 +37,14 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 # fit the model
 model.fit(X_train, y_train, epochs=150, batch_size=2)
-# evaluate the model
+# evaluate the model (Accuracy)
 loss, acc = model.evaluate(X_test, y_test, verbose=0)
 print('Test Accuracy: %.3f' % acc)
-
+# evaluate the model (Recall)
+from sklearn.metrics import recall_score
+recall_score(y, predictions, average='weighted')
 
 predictions = model.predict_classes(X)
 # summarize the first 5 cases
 for i in range(200):
 	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
-
-
-from sklearn.metrics import recall_score
-
-recall_score(y, predictions, average='weighted')
-
